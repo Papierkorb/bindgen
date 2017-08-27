a1da6c8cad2b19aed9314ccdcb9992d70538abbe
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-08-27 14:03:51 +0200

    Don't build clang tool in postinstall, but in tool.sh
    This allows for better integration with shards including generated
    bindings, as the user of that shard doesn't require clang anymore.

--- tool.sh
+++ tool.sh
@@ -4,8 +4,25 @@
 
 BASE="$(dirname "$(readlink -f "$0")")"
 
+EXT_DIR="$BASE/ext/"
 BIN_FILE="$BASE/bin/bindgen"
 SOURCE_FILE="$BASE/src/bindgen.cr"
+
+function print_clang_error {
+  echo "Bindgen requires a full installation of Clang, its libraries and development"
+  echo "headers.  Please install these first, and restart this script."
+  echo "You can also manually run 'make' in ext/ for debugging this issue."
+  echo "Full path to ext/: $EXT_DIR"
+
+  exit 1
+}
+
+if [ ! -f "$EXT_DIR/bindgen" ]; then
+  echo "** ext/bindgen not found.  Building now."
+  cd "$EXT_DIR"
+  make || print_clang_error
+  cd -
+fi
 
 if [ -f "$BIN_FILE" ]; then
   exec "$BIN_FILE" $@
