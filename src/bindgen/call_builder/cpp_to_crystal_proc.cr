f976826e891481cd3f9e96acd4ed8f6e296bd74e
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-09-20 19:21:13 +0200

    CallBuilder::CppToCrystalProc: Finish implementation

--- src/bindgen/call_builder/cpp_to_crystal_proc.cr
+++ src/bindgen/call_builder/cpp_to_crystal_proc.cr
@@ -1,13 +1,13 @@
 module Bindgen
   module CallBuilder
-    # Analyzer for calls made from C++ to Crystal.  Used for callbacks, Qt
+    # Builder for calls made from C++ to Crystal.  Used for callbacks, Qt
     # signals and virtual method overrides.
     class CppToCrystalProc
       def initialize(@db : TypeDatabase)
       end
 
       # Calls the *method*, using the *proc_name* to call-through to Crystal.
-      def build(method : Parser::Method, proc_name : String = "_proc_") : Call
+      def build(method : Parser::Method, proc_name : String) : Call
         pass = Cpp::Pass.new(@db)
 
         arguments = method.arguments.map_with_index do |arg, idx|
@@ -21,8 +21,16 @@ module Bindgen
           name: proc_name,
           result: result,
           arguments: arguments,
-          body: Call::EmptyBody.new,
+          body: Body.new,
         )
+      end
+
+      class Body < Call::Body
+        def to_code(call : Call, platform : Graph::Platform) : String
+          pass_args = call.arguments.map(&.call).join(", ")
+
+          %[#{call.name}(#{pass_args})]
+        end
       end
     end
   end
