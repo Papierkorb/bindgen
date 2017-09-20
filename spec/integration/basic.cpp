ec6bd13fc02a7290551ec7f687aab3b6635bb2bc
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-09-20 19:50:14 +0200

    integration/basic: Test method filtering

--- spec/integration/basic.cpp
+++ spec/integration/basic.cpp
@@ -1,3 +1,5 @@
+class IgnoreMe { };
+
 class Adder {
   int base;
 public:
@@ -10,4 +12,20 @@ public:
   static int sum(int a, int b) {
     return a + b;
   }
+
+  void ignoreByName() { }
+  void ignoreByArgument(IgnoreMe me) { }
+  void ignoreByArgumentRef(IgnoreMe &me) { }
+  void ignoreByArgumentPtr(IgnoreMe *me) { }
+  void ignoreByArgumentCRef(const IgnoreMe &me) { }
+  void ignoreByArgumentCPtr(const IgnoreMe *me) { }
+  IgnoreMe ignoreByReturn() { return IgnoreMe(); }
+  IgnoreMe &ignoreByReturnRef() { return *new IgnoreMe(); }
+  const IgnoreMe &ignoreByReturnCRef() { return *new IgnoreMe(); }
+  IgnoreMe *ignoreByReturnPtr() { return new IgnoreMe(); }
+  const IgnoreMe *ignoreByReturnCPtr() { return new IgnoreMe(); }
+};
+
+struct ImplicitConstructor {
+  int itWorks() { return 1; }
 };
--- spec/integration/basic_spec.cr
+++ spec/integration/basic_spec.cr
@@ -3,12 +3,41 @@ require "./spec_helper"
 describe "a basic C++ wrapper" do
   it "works" do
     build_and_run("basic") do
-      it "supports static methods" do
-        Test::AdderWrap.sum(4, 5).should eq(9)
+      context "core functionality" do
+        it "supports static methods" do
+          Test::AdderWrap.sum(4, 5).should eq(9)
+        end
+
+        it "supports member methods" do
+          Test::AdderWrap.new(4).sum(5).should eq(9)
+        end
       end
 
-      it "supports member methods" do
-        Test::AdderWrap.new(4).sum(5).should eq(9)
+      context "if class has implicit default constructor" do
+        it "has a default constructor" do
+          Test::ImplicitConstructor.new.it_works.should eq(1)
+        end
+      end
+
+      context "crystal wrapper features" do
+        it "adds #initialize(unwrap: Binding::T*)" do
+          {{
+            Test::AdderWrap.methods.any? do |m|
+              m.name == "initialize" && \
+              m.args.size == 1 && \
+              m.args.any? do |a|
+                a.name.stringify == "unwrap"
+              end
+            end
+          }}.should be_true
+        end
+      end
+
+      context "method filtering" do
+        methods = {{ Test::AdderWrap.methods.map(&.name.stringify) }}
+        it "removes argument value type" do
+          methods.includes?("ignoreByArgument").should be_false
+        end
       end
     end
   end
