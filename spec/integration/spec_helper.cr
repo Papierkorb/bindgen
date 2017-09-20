e9e76d0ccd26006c08e21448a2437b81a013c875
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-09-20 19:51:29 +0200

    integration tests: Extract common YAML configuration

--- spec/integration/spec_helper.cr
+++ spec/integration/spec_helper.cr
@@ -9,54 +9,57 @@ SPEC_METHODS = { "it(", "context(", "describe(" }
 # small test skeleton.  Write normal `spec` it-tests in it.
 #
 # This is magic.  Have a look at the `*_spec.cr` files as sample code.
-macro build_and_run(name, offset = __LINE__, source = __FILE__)
-  build_and_run_impl({{ name }}, {{ yield.stringify }}, {{ offset }}, {{ source }})
+macro build_and_run(name, start = __LINE__, stop = __END_LINE__, source = __FILE__)
+  build_and_run_impl({{ name }}, {{ start }}, {{ stop }} - 1, {{ source }})
+end
+
+# Injects a `describe` block just before the first test-case.  This way, we can
+# conveniently define classes first, and specs immediately after that.  The
+# indention depth is pretty large already, so every 2-spaces count visually.
+private def inject_describe_block(code, name)
+  if match = /^ +(?:it|context|describe)[ (]/m.match(code)
+    pos = match.begin(0).not_nil!
+    range = pos...pos
+    code.sub(range, %<describe "Online test of #{name}" do >) + "\nend"
+  else
+    %<describe "Online test of #{name}" do
+        #{code}
+      end>
+  end
 end
 
 # Embeds *code* into a `spec` skeleton.  Automatically requires the generated
 # crystal wrapper of *name*.
 private def embedded_test_code(name, code)
-  <<-EOF
-    require "spec"
+  %<require "spec"
     require "./#{name}"
 
-    describe "Online test of #{name}" do
-      #{code}
-    end
-  EOF
+    Spec.override_default_formatter(Spec::VerboseFormatter.new)
+
+    #{code}>
 end
 
-# Reads *code* line-wise, and inserts magic `#<loc:...>` comments before `spec`
-# methods.  This will get the inner spec command to point to the *file* (The
-# file the user wrote), instead of the generated source.
+# Prepends a `#<loc..>` pragma to *code*, pointing at *file* on line *base*.
+# This will make the online spec point back to the original file.
 private def relocate_line_reports(code, file, base)
-  String.build do |b|
-    offset = 0
-
-    code.each_line(chomp: false) do |line|
-      offset += 1
-
-      if SPEC_METHODS.any?{|x| line.starts_with?(x)}
-        line_no = base + offset
-        b << "#<loc:#{file.inspect},#{line_no},1>\n"
-      end
-
-      b << line
-    end
-  end
+  "#<loc:#{file.inspect},#{base},1>\n#{code}"
 end
 
 # Implementation helper for `.build_and_run`.
-def build_and_run_impl(name, test_code, source_offset, source_file)
+def build_and_run_impl(name, source_start, source_end, source_file)
   config_file = "#{__DIR__}/#{name}.yml"
   test_file = "#{__DIR__}/tmp/#{name}_test.cr"
 
-  # Write live test
-  code = relocate_line_reports(test_code, source_file, source_offset)
+  # Write live test.  Read the source from the input file, so we keep the exact
+  # formatting.  Else, the line reporting will point anywhere, but not where we
+  # want them.
+  code = File.read(source_file).lines[source_start...source_end].join("\n")
+  code = relocate_line_reports(code, source_file, source_start)
+  code = inject_describe_block(code, name)
   File.write(test_file, embedded_test_code(name, code))
 
   # Run bindgen tool
-  config = Bindgen::Configuration.from_yaml File.read(config_file)
+  config = Bindgen::ConfigReader.from_file Bindgen::Configuration, config_file
   tool = Bindgen::Tool.new(__DIR__, config, show_stats: false)
 
   status = nil
@@ -65,11 +68,12 @@ def build_and_run_impl(name, test_code, source_offset, source_file)
 
   # Run the tool and then the test program
   Dir.cd(__DIR__) do
+    ENV["SPEC_NAME"] = name # For later access from the `.yml`
     tool.run!.should eq(0)
     status = Process.run(command, shell: true, output: output, error: output)
   end
 
-  raise "Status shouldn't be nil here" if status.nil?
+  raise "BUG: Status shouldn't be nil here" if status.nil?
 
   # On failure, print the output of the failed spec call.
   unless status.success?
@@ -77,7 +81,7 @@ def build_and_run_impl(name, test_code, source_offset, source_file)
     STDERR.write output.to_slice
     puts "#{"<<< Failed spec command:".colorize.mode(:bold)} #{command}"
 
-    raise Spec::AssertionFailed.new("Test for #{name}.yml failed, see above", source_file, source_offset)
+    raise Spec::AssertionFailed.new("Test for #{name}.yml failed, see above", source_file, source_start)
   end
 
   # If we reach this line, the inner spec was successful - Yay!
