4384cf75c0087bc916cb260414292559de196430
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-09-20 19:30:35 +0200

    Add Crystal::Method

--- src/bindgen/call_builder/crystal_wrapper.cr
+++ src/bindgen/call_builder/crystal_wrapper.cr
@@ -8,7 +8,6 @@ module Bindgen
 
       def build(method : Parser::Method, target : Call)
         pass = Crystal::Pass.new(@db)
-        argument = Crystal::Argument.new(@db)
 
         arguments = pass.arguments_to_wrapper(method.arguments)
         if method.any_constructor?
@@ -33,43 +32,46 @@ module Bindgen
         # Generates the call itself.
         abstract def encapsulate(call, code)
 
+        def encapsulate_into_variable(call, code)
+          "result = " + encapsulate(call, code)
+        end
+
         # Support for pre- and post hooks.
         private def encapsulate_and_hook(call, platform, code)
           pre = @pre_hook
           post = @post_hook
-          code = encapsulate(call, code)
 
           String.build do |b|
             b << pre.to_code(call, platform) << "\n" if pre
             if post
-              b << "result = " << code << "\n"
+              b << encapsulate_into_variable(call, code) << "\n"
               b << post.to_code(call, platform)
-              b << "result" # Implicit return
+              b << "result" unless call.origin.any_constructor?
             else
-              b << code
+              b << encapsulate(call, code)
             end
           end
         end
 
         def to_code(call : Call, platform : Graph::Platform) : String
-          formatter = Crystal::Format.new(@db)
-          typer = Crystal::Typename.new(@db)
-          func_result = typer.full(call.result)
-          func_args = formatter.argument_list(call.arguments)
-
+          method = Crystal::Method.new(@db)
           code = @target.body.to_code(@target, platform)
           code = encapsulate_and_hook(call, platform, code)
 
           # Crystal doesn't like `#initialize` methods having an explicit return
           # type.
-          unless call.origin.any_constructor?
-            suffix = " : #{func_result}"
-          end
+          result = call.result unless call.origin.any_constructor?
 
-          static = "self." if call.origin.static_method?
-          kind = "protected " if call.origin.protected?
+          head_line = method.prototype(
+            name: call.name,
+            arguments: call.arguments,
+            result: result,
+            static: call.origin.static_method?,
+            abstract: call.origin.pure?,
+            protected: call.origin.protected?,
+          )
 
-          %[#{kind}def #{static}#{call.name}(#{func_args})#{suffix}\n] \
+          %[#{head_line}\n] \
           %[  #{code}\n] \
           %[end\n]
         end
@@ -86,6 +88,10 @@ module Bindgen
       end
 
       class ConstructorBody < Body
+        def encapsulate_into_variable(call, code)
+          encapsulate(call, code)
+        end
+
         def encapsulate(call, code)
           %[result = #{code}\n] \
           %[@unwrap = result]
---
+++ src/bindgen/crystal/method.cr
@@ -0,0 +1,25 @@
+module Bindgen
+  module Crystal
+    # Functionality to generate method code.
+    struct Method
+      def initialize(@db : TypeDatabase)
+      end
+
+      # Builds the prototype ("first line") of a method.
+      def prototype(name, arguments, result = nil, static = false, abstract abstract_ = false, protected protected_ = false)
+        formatter = Format.new(@db)
+        typer = Typename.new(@db)
+        func_result = typer.full(result) if result
+        func_args = formatter.argument_list(arguments)
+
+        kind = ""
+        suffix = " : #{func_result}" if func_result
+        name_prefix = "self." if static
+        kind = "protected " if protected_
+        kind += "abstract " if abstract_
+
+        %[#{kind}def #{name_prefix}#{name}(#{func_args})#{suffix}]
+      end
+    end
+  end
+end
