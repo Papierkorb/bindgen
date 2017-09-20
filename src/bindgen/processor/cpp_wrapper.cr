be7725ee9f5969502d9a6d2a8590f8d93a2f7f42
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-09-20 19:39:04 +0200

    Processor::CppWrapper: Skip c'tor in abstract class w/o shadow-subclass

--- src/bindgen/processor/cpp_wrapper.cr
+++ src/bindgen/processor/cpp_wrapper.cr
@@ -13,10 +13,18 @@ module Bindgen
       def visit_method(method)
         return if method.calls[PLATFORM]?
 
+        klass = method.parent_class # Skip constructors on abstract class without shadow-subclass.
+        return if klass.try(&.origin.abstract?) && method.origin.any_constructor? && !klass.try(&.cpp_sub_class)
+
         call = CallBuilder::CppCall.new(@db)
         wrapper = CallBuilder::CppWrapper.new(@db)
         target = call.build(method.origin)
-        method.calls[PLATFORM] = wrapper.build(method.origin, target)
+
+        method.calls[PLATFORM] = wrapper.build(
+          method: method.origin,
+          class_name: method.origin.class_name,
+          target: target,
+        )
       end
     end
   end
