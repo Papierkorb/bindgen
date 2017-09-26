b94cc5a21e987881f0262bf1b553e3c975ed8b14
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-09-26 21:51:59 +0200

    Add integration test for container instantiation feature

---
+++ spec/integration/containers.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+
+class Containers {
+public:
+  std::vector<int> integers() {
+    return std::vector<int>{ 1, 2, 3 };
+  }
+
+  std::vector<std::string> strings() {
+    return std::vector<std::string>{ "One", "Two", "Three" };
+  }
+
+  double sum(std::vector<double> list) {
+    double d = 0;
+
+    for (double c : list) {
+      d += c;
+    }
+
+    return d;
+  }
+};
