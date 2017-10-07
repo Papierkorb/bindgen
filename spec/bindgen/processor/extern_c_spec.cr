661de0da25cdc9dda3b7fec2137689304bc90963
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-10-07 18:50:47 +0200

    Processor::ExternC: No more C++ wrapper methods for pure C functions

---
+++ spec/bindgen/processor/extern_c_spec.cr
@@ -0,0 +1,207 @@
+require "../../spec_helper"
+
+private def argument(name, type)
+  Bindgen::Parser::Argument.new(name, Bindgen::Parser::Type.parse(type))
+end
+
+describe Bindgen::Processor::ExternC do
+  config = Bindgen::Configuration.from_yaml <<-YAML
+  module: Foo
+  generators: { }
+  parser: { files: [ "foo.h" ] }
+  YAML
+
+  doc = Bindgen::Parser::Document.new
+  db = Bindgen::TypeDatabase.new(Bindgen::TypeDatabase::Configuration.new, "boehmgc-cpp")
+  db.add("HasToCpp", to_cpp: "TO_CPP", copy_structure: true)
+  db.add("HasFromCpp", from_cpp: "FROM_CPP", copy_structure: true)
+  db.add("PassByValue", pass_by: Bindgen::TypeDatabase::PassBy::Reference)
+
+  extern_c_void_func = Bindgen::Parser::Method.new(
+    name: "foo",
+    className: "",
+    arguments: [ ] of Bindgen::Parser::Argument,
+    returnType: Bindgen::Parser::Type::VOID,
+    isExternC: true,
+  )
+
+  void_result = Bindgen::Cpp::Pass.new(db).to_cpp(Bindgen::Parser::Type::VOID)
+
+  dummy_call = Bindgen::Call.new(
+    name: "foo",
+    result: void_result,
+    arguments: [ ] of Bindgen::Call::Argument,
+    body: Bindgen::Call::EmptyBody.new,
+    origin: extern_c_void_func,
+  )
+
+  subject = Bindgen::Processor::ExternC.new(config, db)
+
+  it "requires the method to use the C ABI" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: Bindgen::Parser::Method.new(
+        name: "foo",
+        className: "",
+        arguments: [ ] of Bindgen::Parser::Argument,
+        returnType: Bindgen::Parser::Type::VOID,
+        isExternC: false,
+      )
+    )
+
+    subject.process(graph, doc)
+
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "requires the method to have no to_cpp arguments (explicit)" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: Bindgen::Parser::Method.new(
+        name: "foo",
+        className: "",
+        arguments: [
+          argument("a", "int"),
+          argument("b", "const char *"),
+          argument("c", "HasToCpp"),
+        ],
+        returnType: Bindgen::Parser::Type::VOID,
+        isExternC: true,
+      )
+    )
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "requires the method to have no to_cpp arguments (implicit)" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: Bindgen::Parser::Method.new(
+        name: "foo",
+        className: "",
+        arguments: [
+          argument("a", "int"),
+          argument("b", "const char *"),
+          argument("c", "PassByValue *"),
+        ],
+        returnType: Bindgen::Parser::Type::VOID,
+        isExternC: true,
+      )
+    )
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "requires the methods return type to have no from_cpp (explicit)" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: Bindgen::Parser::Method.new(
+        name: "foo",
+        className: "",
+        arguments: [
+          argument("a", "int"),
+          argument("b", "const char *"),
+        ],
+        returnType: Bindgen::Parser::Type.parse("HasFromCpp"),
+        isExternC: true,
+      )
+    )
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "requires the methods return type to have no from_cpp (implicit)" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: Bindgen::Parser::Method.new(
+        name: "foo",
+        className: "",
+        arguments: [
+          argument("a", "int"),
+          argument("b", "const char *"),
+        ],
+        returnType: Bindgen::Parser::Type.parse("PassByValue *"),
+        isExternC: true,
+      )
+    )
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "skips methods having a CrystalBinding call" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: extern_c_void_func,
+    )
+
+    method.calls[Bindgen::Graph::Platform::CrystalBinding] = dummy_call
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "skips methods having a Cpp call" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: extern_c_void_func,
+    )
+
+    method.calls[Bindgen::Graph::Platform::Cpp] = dummy_call
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should be_nil
+  end
+
+  it "skips methods having EXPLICIT_BIND_TAG set" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: extern_c_void_func,
+    )
+
+    method.set_tag(Bindgen::Graph::Method::EXPLICIT_BIND_TAG, "unit-test")
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should eq("unit-test")
+  end
+
+  it "adds the EXPLICIT_BIND_TAG otherwise" do
+    graph = Bindgen::Graph::Namespace.new("ROOT")
+    method = Bindgen::Graph::Method.new(
+      name: "foo",
+      parent: graph,
+      origin: Bindgen::Parser::Method.new(
+        name: "libfoo_bar",
+        className: "",
+        arguments: [
+          argument("a", "int"),
+          argument("b", "const char *"),
+          argument("c", "HasFromCpp"),
+        ],
+        returnType: Bindgen::Parser::Type.parse("HasToCpp"),
+        isExternC: true,
+      )
+    )
+
+    subject.process(graph, doc)
+    method.tag?(Bindgen::Graph::Method::EXPLICIT_BIND_TAG).should eq("libfoo_bar")
+  end
+end
---
+++ src/bindgen/processor/extern_c.cr
@@ -0,0 +1,52 @@
+module Bindgen
+  module Processor
+    # Checks if a method require a C/C++ wrapper.  If not, marks the method to
+    # bind directly to the target method instead of writing a "trampoline"
+    # wrapper in C++.
+    #
+    # A method can be bound directly if all of these are true:
+    #
+    # 1. It uses the C ABI (`extern "C"`)
+    # 2. No argument uses a `to_cpp` converter
+    # 3. The return type doesn't use a `from_cpp` converter
+    #
+    # Additionally, these rules must be met:
+    #
+    # 4. No calls for `CrystalBinding` nor `Cpp` are set
+    # 5. `Graph::Method::EXPLICIT_BIND_TAG` is not already set
+    #
+    # If any of this is false, the method is left alone.
+    class ExternC < Base
+      PLATFORM = Graph::Platform::Crystal
+
+      def visit_platform_specific(specific)
+        super if specific.platforms.includes?(PLATFORM)
+      end
+
+      def visit_method(method)
+        return unless method.origin.extern_c? # Rule 1
+
+        return if method.tag?(Graph::Method::EXPLICIT_BIND_TAG) # Rule 5
+        unless method.tag?(Graph::Method::REMOVABLE_BINDING_TAG) # Rule 4
+          return if method.calls[Graph::Platform::CrystalBinding]?
+          return if method.calls[Graph::Platform::Cpp]?
+        end
+
+        # Conversion checks
+        pass = Cpp::Pass.new(@db)
+        any_arg_uses_conversion = method.origin.arguments.any? do |arg|
+          pass.to_cpp(arg).conversion != nil
+        end
+
+        return if pass.to_crystal(method.origin.return_type).conversion != nil # Rule 3
+        return if any_arg_uses_conversion # Rule 2
+
+        # If we end up here, the method can be bound to directly.
+        method.set_tag(Graph::Method::EXPLICIT_BIND_TAG, method.origin.name)
+
+        method.calls.delete Graph::Platform::CrystalBinding
+        method.calls.delete Graph::Platform::Cpp
+      end
+    end
+  end
+end
