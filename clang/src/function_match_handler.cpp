f64ed4fd1d6f91b94aecdf544f4b3fd418da8d72
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-10-07 19:24:31 +0200

    clang: Check if function is in extern "C" context

--- clang/src/function_match_handler.cpp
+++ clang/src/function_match_handler.cpp
@@ -38,7 +38,6 @@ static std::string getFunctionParentName(const std::string &fullName, const std:
 }
 
 static Method buildMethod(const clang::FunctionDecl *func, const std::string &fullName) {
-
 	Method m;
 	m.name = func->getNameAsString();
 	m.className = getFunctionParentName(fullName, m.name);
@@ -46,12 +45,13 @@ static Method buildMethod(const clang::FunctionDecl *func, const std::string &fu
 	m.isConst = false;
 	m.isVirtual = false;
 	m.isPure = false;
-	m.isExternC = func->isExternC();
+	m.isExternC = func->isExternC() || func->isInExternCContext();
 	m.access = clang::AS_public; // Global functions are always public
 
 	clang::ASTContext &ctx = func->getASTContext();
 	m.returnType = TypeHelper::qualTypeToType(func->getReturnType(), ctx);
 	TypeHelper::addFunctionParameters(func, m);
+
 	return m;
 }
 
@@ -59,7 +59,6 @@ void FunctionMatchHandler::runOnFunction(const clang::FunctionDecl *func) {
 	std::string fullName = func->getQualifiedNameAsString();
 
 	if (isFunctionInteresting(fullName)) {
-
 		this->m_functions.push_back(buildMethod(func, fullName));
 	}
 }
