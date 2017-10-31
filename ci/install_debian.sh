707142d7a464bb54a1f5c3bd946cd8928af8d5d1
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-10-31 14:06:37 +0100

    Move everything CI related to ci/

--- ci/install_debian.sh
+++ ci/install_debian.sh
@@ -17,9 +17,14 @@ cat <<EOF > /etc/apt/sources.list.d/crystal.list
 deb https://dist.crystal-lang.org/apt crystal main
 EOF
 
-apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54
-
 apt-get update
+
+if [ ! -f /etc/lsb-release ]; then
+  # Hack to detect Debian, which doesn't ship with a GPG installation.
+  apt-get install gnupg --yes
+fi
+
+apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54
 apt-get install --yes --allow-unauthenticated \
   build-essential \
   crystal libxml2-dev zlib1g-dev libncurses-dev libgc-dev libyaml-dev \
