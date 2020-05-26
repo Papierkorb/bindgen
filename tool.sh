8732a9093ba303fb1ae19606075a80ffc65f5e74
Davor Ocelic <docelic@crystallabs.io>
2020-05-27 01:23:52 +0200

    Use new build system in tool.sh

--- tool.sh
+++ tool.sh
@@ -10,7 +10,7 @@ SOURCE_FILE="$BASE/src/bindgen.cr"
 function print_clang_error {
   echo "  Bindgen requires a full installation of Clang, its libraries and development"
   echo "  headers.  Please install these first, and restart this script."
-  echo "  You can also manually run 'make' in clang/ for debugging this issue."
+  echo "  You can also manually run 'cmake .; make -j' in clang/ for debugging this issue."
   echo "  Full path to clang/: $CLANG_DIR"
 
   exit 1
@@ -19,7 +19,7 @@ function print_clang_error {
 if [ ! -f "$CLANG_DIR/bindgen" ]; then
   echo "** clang/bindgen not found.  Building now."
   cd "$CLANG_DIR"
-  make || print_clang_error
+  ( cmake . && make -j ) || print_clang_error
   cd -
 fi
 
