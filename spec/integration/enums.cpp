d7dcf4b9b8318ba688ae2d1b56baae10250bca96
Quinton Miller <nicetas.c@gmail.com>
2020-10-06 16:28:45 +0800

    integration tests for Enums processor

---
+++ spec/integration/enums.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+
+enum TopLevel {
+  A,
+  B = A,
+  C = -1,
+};
+
+namespace NS1 {
+  enum InnerEnum { X };
+
+  namespace NS2 {
+    enum InnerEnum2 { X };
+  }
+}
+
+enum U8Enum : uint8_t {
+  D,
+  E = (uint8_t)-1,
+};
+
+
+
+template <typename T>
+class QFlags { };
+
+enum FlagEnum {
+  None = 0x00,
+  P = 0x01,
+  Q = 0x04,
+  R = 0x0C,
+};
+
+template class QFlags<FlagEnum>;
+typedef QFlags<FlagEnum> Flags;
