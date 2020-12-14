6da2f3a881fb6b40ee03d137fc801f5e15aa8061
Frank Fischer <frank-fischer@shadow-soft.de>
2020-12-14 14:55:43 +0100

    Remove deprecated "json_mapping" and "yaml_mapping"

--- src/bindgen/library.cr
+++ src/bindgen/library.cr
@@ -2,9 +2,7 @@
 # Require this file if you want to use bindgen as library.
 
 require "json"
-require "json_mapping"
 require "yaml"
-require "yaml_mapping"
 require "set"
 require "weak_ref"
 require "colorize"
